fd4316a6445f3edf20b9546753bfb00b593e0032
GigaHierz <lenahierzi@hotmail.de>
2020-11-14 21:03:47 +0100

    Add wedding pictures

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ var slidesGreece = document.querySelectorAll('#greece .slide');
 var currentSlideGreece = 0;
 var slideIntervalGreece = setInterval(nextSlideGreece,5000);
 
+var slidesWedding = document.querySelectorAll('#wedding .slide');
+var currentSlideWedding = 0;
+var slideIntervalWedding = setInterval(nextSlidewedding,2000);
+
+var slidesWedding2 = document.querySelectorAll('#wedding2 .slide');
+var currentSlideWedding2 = 0;
+var slideIntervalWedding2 = setInterval(nextSlidewedding2,2000);
+
 
 function nextSlide() {
    slides[currentSlide].className = 'slide';
@@ -16,4 +24,15 @@ function nextSlideGreece() {
    slidesGreece[currentSlideGreece].className = 'slide';
    currentSlideGreece = (currentSlideGreece+1)%slidesGreece.length;
    slidesGreece[currentSlideGreece].className = 'slide showing';
+
+}
+function nextSlidewedding() {
+   slidesWedding[currentSlideWedding].className = 'slide';
+   currentSlideWedding = (currentSlideWedding+1)%slidesWedding.length;
+   slidesWedding[currentSlideWedding].className = 'slide showing';
+}
+function nextSlidewedding2() {
+   slidesWedding2[currentSlideWedding2].className = 'slide';
+   currentSlideWedding2 = (currentSlideWedding2+1)%slidesWedding2.length;
+   slidesWedding2[currentSlideWedding2].className = 'slide showing';
 }
